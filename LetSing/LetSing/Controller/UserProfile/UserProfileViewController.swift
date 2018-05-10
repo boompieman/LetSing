617c5e266700710d0c9d2d3f9764da8be80e091b
Boompieman <t0915290092@gmail.com>
2018-05-10 20:10:08 +0800

    change branch to test recoring func

--- LetSing/LetSing/Controller/UserProfile/UserProfileViewController.swift
+++ LetSing/LetSing/Controller/UserProfile/UserProfileViewController.swift
@@ -6,18 +6,42 @@
 //  Copyright © 2018年 MACBOOK. All rights reserved.
 //
 
-import Foundation
+import AVFoundation
 import UIKit
 import YouTubePlayer
 
 
-class userProfileViewController: UIViewController, YouTubePlayerDelegate {
+class userProfileViewController: UIViewController, YouTubePlayerDelegate, AVAudioRecorderDelegate {
+
+
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var recordButton: UIButton!
 
     @IBOutlet weak var YoutubeView: YouTubePlayerView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        recordingSession = AVAudioSession.sharedInstance()
+
+        do {
+            print("aaa")
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("user allows to use mic")
+                        self.loadRecordingUI()
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+
 
 //        YoutubeView.isUserInteractionEnabled = false
 
@@ -26,7 +50,7 @@ class userProfileViewController: UIViewController, YouTubePlayerDelegate {
         YoutubeView.playerVars = ["playsinline": 1 as AnyObject,
                                   "showinfo": 0 as AnyObject,
                                   "controls": 1 as AnyObject,
-                                  
+
 //                                  "start": 120 as AnyObject, // 單位：sec
 //                                  "autoplay": 1 as AnyObject,
                                   "iv_load_policy": 3 as AnyObject,
@@ -38,6 +62,70 @@ class userProfileViewController: UIViewController, YouTubePlayerDelegate {
 
         YoutubeView.loadVideoID("T0LfHEwEXXw")
 
+    }
+
+    func loadRecordingUI() {
+        recordButton = UIButton(frame: CGRect(x: 64, y: 300, width: 128, height: 64))
+        recordButton.setTitle("Tap to Record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.title1)
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        view.addSubview(recordButton)
+    }
+
+    @objc private func recordTapped() {
+
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        print(paths[0])
+
+        return paths[0]
+    }
+
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
+
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
     }
 
     func playerReady(_ videoPlayer: YouTubePlayerView) {
