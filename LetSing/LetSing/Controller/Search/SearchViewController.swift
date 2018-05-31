b8c522b4c3806f7adb29aa483ce4ea5fc145451c
boompieman <t0915290092@gmail.com>
2018-05-31 12:33:32 +0800

    merge with analytics branch

--- LetSing/LetSing/Controller/Search/SearchViewController.swift
+++ LetSing/LetSing/Controller/Search/SearchViewController.swift
@@ -37,18 +37,6 @@ class SearchViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = false
 
         songManager.delegate = self
-
-        let button = UIButton(type: .roundedRect)
-        button.frame = CGRect(x: 20, y: 100, width: 100, height: 30)
-        button.setTitle("Crash", for: [])
-        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-        view.addSubview(button)
-
-
-    }
-
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-        Crashlytics.sharedInstance().crash()
     }
 
 
