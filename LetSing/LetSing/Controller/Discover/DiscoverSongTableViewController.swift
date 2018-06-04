1c60d9a37d6ea6fa6e17f4a3aade98ca8bee30b4
boompieman <t0915290092@gmail.com>
2018-06-04 14:53:41 +0800

    switch to refactor_discover branch

---
+++ LetSing/LetSing/Controller/Discover/DiscoverSongTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DiscoverTableViewController.swift
+//  LetSing
+//
+//  Created by MACBOOK on 2018/6/4.
+//  Copyright © 2018年 MACBOOK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DiscoverSongTableViewController: UIViewController {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
