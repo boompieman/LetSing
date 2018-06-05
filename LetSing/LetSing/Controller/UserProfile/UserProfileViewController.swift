3571c479b4e121299e939d17e5b701b63024ddea
boompieman <t0915290092@gmail.com>
2018-06-05 12:18:55 +0800

    seperate profileView and tableView by using container view

--- LetSing/LetSing/Controller/UserProfile/UserProfileViewController.swift
+++ LetSing/LetSing/Controller/UserProfile/UserProfileViewController.swift
@@ -12,7 +12,6 @@ import UIKit
 class userProfileViewController: UIViewController {
 
     @IBOutlet weak var userInfoView: UserInfoView!
-    @IBOutlet weak var tableView: UITableView!
 
     var records = [Record]()
 
@@ -20,13 +19,7 @@ class userProfileViewController: UIViewController {
         super.viewDidLoad()
 
         requestProfile()
-        setupTableView()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.records = LSRecordFileManager.shared.fetchAllRecords()
-        self.tableView.reloadData()
+        
     }
 
     func requestProfile() {
@@ -35,107 +28,6 @@ class userProfileViewController: UIViewController {
             self?.userInfoView.updateProfileWith(name: user.name, image: user.image)
         }) { (error) in
             print(error)
-        }
-    }
-
-    func setupTableView() {
-
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        let nib = UINib(nibName: String(describing: UserVideoTableViewCell.self), bundle: nil)
-
-        self.tableView.register(nib, forCellReuseIdentifier: String(describing: UserVideoTableViewCell.self))
-
-        tableView.contentInset = LSConstants.tableViewInset
-    }
-}
-
-extension userProfileViewController: UITableViewDelegate, UITableViewDataSource {
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 1
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return self.records.count
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-
-        return 150
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let tableViewCell = tableView.dequeueReusableCell(
-            withIdentifier: String(describing: UserVideoTableViewCell.self),
-            for: indexPath
-            ) as! UserVideoTableViewCell
-
-        tableViewCell.titleLabel.text = self.records[indexPath.row].title
-
-        print(self.records[indexPath.row].videoUrl.absoluteString.components(separatedBy: "/"))
-
-        return tableViewCell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        performSegue(withIdentifier: String(describing: VideoPlayerViewController.self), sender: indexPath)
-    }
-
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-
-    }
-
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        var actionArray = [UITableViewRowAction]()
-
-        let actionEdit = UITableViewRowAction(style: .default, title:"編輯") { (action, indexPath) in
-
-            let alert = AlertManager.shared.showEdit(
-                with: "編輯標題",
-                message: nil,
-                placeholder: "請輸入標題..",
-                completion: { (text) in
-                    LSRecordFileManager.shared.updateRecordTitle(from: self.records[indexPath.row].videoUrl, to: text)
-                    self.records[indexPath.row].title = text
-                    self.tableView.isEditing = false
-                    self.tableView.reloadData()
-            })
-
-            self.present(alert, animated: true, completion: nil)
-        }
-
-        actionEdit.backgroundColor = UIColor.orange
-
-        let actionDelete = UITableViewRowAction(style: .default, title: "刪除") { (action, indexPath) in
-            LSRecordFileManager.shared.deleteRecord(at: self.records[indexPath.row].videoUrl)
-            self.records.remove(at: indexPath.row)
-            tableView.isEditing = false
-            tableView.reloadData()
-        }
-
-        actionDelete.backgroundColor = UIColor.red
-        actionArray.append(actionDelete)
-        actionArray.append(actionEdit)
-
-        return actionArray
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-        guard let identifier = segue.identifier else { return }
-
-        if identifier == String(describing: VideoPlayerViewController.self) {
-            
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-
-                let destinationVC = segue.destination as? VideoPlayerViewController
-                destinationVC?.videoURL = self.records[indexPath.row].videoUrl
-            }
         }
     }
 }
